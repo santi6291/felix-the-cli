ddbba6676be8df5219b2ec3de66f64736af1bd33
Santiago J Sosa <me@santisosa.com>
2018-11-11 21:25:00 -0500

    Properly clear temp directory

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -1,24 +1,36 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as os from 'os';
+// import * as rimraf from 'rimraf';
+const rimraf = require('rimraf');
+// import { rimraf } from 'rimraf';
 
+
+import { expect } from 'chai';
+import 'mocha';
 
 import InitSpecs from './commands/init/init.spec';
 
 class TestEnv {
-    constructor() {
-        fs.mkdtemp(`${os.tmpdir()}${path.sep}`, this.initTest.bind(this))
-    }
 
-    private initTest(err: Error, folder: string){
+    constructor() {
+        console.log('Creating Sandbox Directory');
         process.env['APP_DIR'] = path.resolve(__dirname, '../');
-        process.env['OUTPUT_DIR'] = folder;
-        console.log('OUTPUT_DIR: ', folder);
+        process.env['OUTPUT_DIR'] = fs.mkdtempSync(`${os.tmpdir()}${path.sep}`);
+        console.log('OUTPUT_DIR: ', process.env['OUTPUT_DIR']);
 
         new InitSpecs();
 
-        console.log('Destroying Sandbox Directory');
-        fs.rmdirSync(<string>process.env['OUTPUT_DIR']);
+        describe('Destroying Sandbox Directory', this.clearTmpDir.bind(this));
+
+    }
+
+    clearTmpDir() {
+        it('Should Destroy Tmp Dir', ()=> {
+            rimraf(process.env['OUTPUT_DIR'],(error: Error)=>{
+                if(error) throw error;
+            });
+        });
     }
 }
 
