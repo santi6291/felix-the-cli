6004e89a7a4972e9f1d2edf2695793282cdc9dc4
Santiago J Sosa <me@santisosa.com>
2018-11-11 19:59:39 -0500

    minor fixes to template service

--- src/services/template/template.ts
+++ src/services/template/template.ts
@@ -10,32 +10,25 @@ export class TemplateService {
     // private path: string;
     private templateStr: string = '';
     private data: any = {};
-    private _rootDir: any = {};
     private options: IConfigFE.ServiceItem;
 
     constructor(options: IConfigFE.ServiceItem) {
         this.options = options;
     }
 
-    public set name(name : string) {
-        this.data['name'] = name;
+    public set name(name : string | undefined) {
+        // remove extension
+        this.data['name'] = name
     }
 
     public compile() {
-        if (!this._rootDir) throw 'Root Directory must be set';
         const path = this.fullPath(this.options.template);
         this.templateStr = fs.readFileSync(path, 'utf8');
         return Mustache.render(this.templateStr, this.data);
     }
 
     private fullPath(pathStr: string): string {
-        // if (!process.env['APP_DIR']) throw 'APP_DIR not set';
+        if (!process.env['APP_DIR']) throw 'APP_DIR not set';
         return path.resolve(<string>process.env['APP_DIR'], pathStr);
     }
-
-    // private getTemplate(err: Error, data:string|Buffer): string|Buffer {
-    //     console.log(data);
-    //     this.templateStr = data;
-    //     return data;
-    // }
 }
