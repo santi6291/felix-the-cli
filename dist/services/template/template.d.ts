850b8fcefce3cbfbf6bafb1d137aab2c25e6bb3b
Santiago J Sosa <me@santisosa.com>
2018-12-04 18:01:45 -0500

    ignore built dir
    Will package this up with NPM

--- dist/services/template/template.d.ts
+++
@@ -1,7 +0,0 @@
-import { IConfigFE } from '../../types/config.d';
-export declare class TemplateService {
-    private options;
-    constructor(options: IConfigFE.ServiceItem);
-    compile(data: any): string;
-    private fullPath;
-}
